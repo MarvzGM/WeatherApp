21578924672a261c301f1d17e12ef5237499840e
MarvzGM <marvzgm@gmail.com>
2018-05-20 07:10:33 +0800

    Added Dangerous Permission

--- app/src/main/java/com/example/marvinchua/weatherapp/ActivityMain.java
+++ app/src/main/java/com/example/marvinchua/weatherapp/ActivityMain.java
@@ -1,8 +1,12 @@
 package com.example.marvinchua.weatherapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -18,6 +22,13 @@ public class ActivityMain extends AppCompatActivity
         binding.button.setOnClickListener(view -> {
             throw new RuntimeException("This is a crash");
         });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat
+                    .requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+        }
         //        setContentView(R.layout.activity_main);
     }
 }
